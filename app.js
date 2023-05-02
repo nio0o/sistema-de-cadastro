503ef6a08d4477700c5a9c5c9642c9e5bae3f82b
João Victor <74017086+nio0o@users.noreply.github.com>
2023-05-01 23:25:54 -0300

    Voltando para o estado original

--- app.js
+++ app.js
@@ -1,6 +1,1 @@
 console.log("Rodando o sisteema de gerenciamento de jogos");
-console.log("Aplicação esta se conectando ao banco de dados");
-
-consolelog("apagueme");
-console.log("enviando pora o git");
-
